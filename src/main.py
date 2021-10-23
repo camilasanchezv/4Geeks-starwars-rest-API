6e7a63ab43d35ef46697027c1082b21da4181671
Camila <contactcamilasanchez@gmail.com>
2021-10-23 02:37:15 +0000

    First commit.

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Character
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,24 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def handle_users():
+    query = User.query.all()
+    users = list(map(lambda x: x.serialize(), query))
+    return jsonify(users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+    query = Planet.query.all()
+    planets = list(map(lambda x: x.serialize(), query))
+    return jsonify(planets), 200
 
-    return jsonify(response_body), 200
+@app.route('/characters', methods=['GET'])
+def handle_characters():
+    query = Character.query.all()
+    characters = list(map(lambda x: x.serialize(), query))
+    return jsonify(characters), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -9,11 +10,49 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    climate = db.Column(String(250), nullable=False)
+    population = db.Column(Integer, nullable=False)
+    orbital_period = db.Column(Integer, nullable=False)
+    diameter = db.Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    birth_year = db.Column(String(250), nullable=False)
+    height = db.Column(Integer, nullable=False)
+    skin_color = db.Column(String(100), nullable=False)
+    eye_color = db.Column(String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
             # do not serialize the password, its a security breach
         }
